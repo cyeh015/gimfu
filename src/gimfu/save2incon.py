fa7d84212240f463f966fcea1ef2434e35bea324
Angus Yeh <angusyeh@gmail.com>
2025-11-19 12:51:28 +1300

    Add reset porosity for save2incon: -reset_porosity

--- src/gimfu/save2incon.py
+++ src/gimfu/save2incon.py
@@ -5,21 +5,24 @@ from t2incons import *
 
 def main():
     if len(argv) < 2:
-        print('use: save2incon a.save b.incon [-reset_kcyc]')
+        print('use: save2incon a.save b.incon [-reset_kcyc] [-reset_porosity]')
         exit(1)
 
     readFrom = argv[1]
     saveTo = argv[2]
 
     if len(argv) > 3:
-        opt = argv[3]
+        opts = argv[3:]
     else:
-        opt = ''
+        opts = []
     inc = t2incon(readFrom)
 
-    if opt == '-reset_kcyc':
-        inc.timing['kcyc'] = 1
-        inc.timing['iter'] = 1
+    for opt in opts:
+        if opt == '-reset_kcyc':
+            inc.timing['kcyc'] = 1
+            inc.timing['iter'] = 1
+        if opt == '-reset_porosity':
+            inc.porosity = None
 
     inc.write(saveTo)
 
